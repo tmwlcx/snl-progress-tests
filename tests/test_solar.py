303e7a01accb980bb13b506a3b78b4c928271746
Tim Wilcox <tim@Mac.tank>
2025-03-17 23:56:34 -0600

    added dummy test for mod_solar

---
+++ tests/test_solar.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestSolar(unittest.TestCase):
+    """Test cases for mod_solar.py module"""
+    
+    def test_dummy(self):
+        """Placeholder test that needs to be implemented"""
+        raise NotImplementedError("Solar module tests not yet implemented")
+
+if __name__ == '__main__':
+    unittest.main()
